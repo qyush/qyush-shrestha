33cc90454eb90113a40a143c8cec546c649389c6
qyush <12201963@students.koi.edu.au>
2023-12-09 19:26:06 +1100

    task 4 changes

--- helloworld/helloworld/Program.cs
+++ helloworld/helloworld/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        double num1, num2, sum;
+        double num1, num2, sum, diff, mul;
 
         Console.Write("Enter the first number: ");
         num1 = Convert.ToDouble(Console.ReadLine());
@@ -13,10 +13,14 @@ class Program
         num2 = Convert.ToDouble(Console.ReadLine());
 
         sum = num1 + num2;
-       
+        diff = num1 - num2;
+        mul = num1 * num2;
+
 
         Console.WriteLine($"The sum of {num1} and {num2} is: {sum}");
-   
+        Console.WriteLine($"The difference of {num1} and {num2} is: {diff}");
+        Console.WriteLine($"The multiplication of {num1} and {num2} is: {mul}");
+
 
         Console.ReadLine();
     }
