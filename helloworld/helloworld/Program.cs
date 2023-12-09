515b32d8c693213cbf1b9d274be87f92317798ff
qyush <12201963@students.koi.edu.au>
2023-12-09 19:23:06 +1100

    task 3

---
+++ helloworld/helloworld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double num1, num2, sum;
+
+        Console.Write("Enter the first number: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        sum = num1 + num2;
+       
+
+        Console.WriteLine($"The sum of {num1} and {num2} is: {sum}");
+   
+
+        Console.ReadLine();
+    }
+}
